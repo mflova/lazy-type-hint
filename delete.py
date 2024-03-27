e2217ec5cc446803b12556b0acbd8de57989c083
LGS-NET\FMAN <manuel.floriano-vazquez@hexagon.com>
2024-03-27 16:00:35 +0100

    fix: removed unused files

--- delete.py
+++
@@ -1,8 +0,0 @@
-from typing import FrozenSet
-
-from dynamic_pyi_generator.data_type_tree import DataTypeTree
-
-a: FrozenSet[int] = {1, 2}
-
-data = [1, 2, 3]
-print(DataTypeTree.get_data_type_tree_for_type(type(data)))
